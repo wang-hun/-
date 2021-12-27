0bdebb660d3484e4630bca6294fca0539a17db32
wang-hun <64296828+wang-hun@users.noreply.github.com>
2021-12-27 22:46:55 +0800

    Add files via upload

---
+++ 扑克牌/扑克牌/MYform.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： WH
+ * 日期: 2021/12/27
+ * 时间: 21:12
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace 扑克牌游戏
+{
+	/// <summary>
+	/// Description of MYform.
+	/// </summary>
+	public partial class MYform : Form
+	{
+		public MYform()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void HelpquitMouseClick(object sender, MouseEventArgs e)
+		{
+			Close();
+		}
+	}
+}
